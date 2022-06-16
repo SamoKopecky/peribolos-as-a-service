a9229ce1dd489015576861c27507c0acbbe708cf
SamoKopecky <skopecky@redhat.com>
2022-06-16 17:05:29 +0200

    feat(controller): Add issue creation on taskRun fail
    Signed-off-by: SamoKopecky <skopecky@redhat.com>

--- src/app.ts
+++ src/app.ts
@@ -30,6 +30,101 @@ const generateTaskPayload = (name: string, context: any) => ({
   },
 });
 
+const getTaskName = async (taskRun: Promise<any>) =>
+  (await taskRun).body.metadata.name;
+
+const checkTaskStatus = async (
+  taskName: string,
+  log: any,
+  errorCountLimit: number = 50,
+  repeatPeriod: number = 1000
+) => {
+  var errorCount = 0;
+  return new Promise((taskRunResultHandler) => {
+    const id = setInterval(async () => {
+      errorCount++;
+      let reason = '';
+
+      // Get task status
+      const taskInfo: any = await useApi(
+        APIS.CustomObjectsApi
+      ).getNamespacedCustomObjectStatus(
+        'tekton.dev',
+        'v1beta1',
+        getNamespace(),
+        'taskruns',
+        taskName
+      );
+
+      try {
+        reason = taskInfo.body.status.conditions[0].reason;
+      } catch (e) {
+        log.error(e);
+      }
+
+      if (reason === 'Failed' || errorCount >= errorCountLimit) {
+        taskRunResultHandler(true);
+        clearInterval(id);
+      } else if (reason === 'Succeeded') {
+        taskRunResultHandler(false);
+        clearInterval(id);
+      }
+    }, repeatPeriod);
+  });
+};
+
+const handleTaskRunResult = async (
+  failed: boolean,
+  taskName: string,
+  log: any,
+  context: any
+) => {
+  if (failed === true) {
+    log.warn(taskName + ' failed.');
+
+    // Used for org name
+    const appSecret = await useApi(APIS.CoreV1Api).readNamespacedSecret(
+      getTokenSecretName(context),
+      getNamespace()
+    );
+
+    // Used for pod name
+    const taskRunObject: any = await useApi(
+      APIS.CustomObjectsApi
+    ).getNamespacedCustomObject(
+      'tekton.dev',
+      'v1beta1',
+      getNamespace(),
+      'taskruns',
+      taskName
+    );
+
+    // Get taskRun logs
+    const podObject = await useApi(APIS.CoreV1Api).readNamespacedPodLog(
+      taskRunObject.body.status.podName,
+      getNamespace()
+    );
+
+    context.octokit.issues.create({
+      owner: Buffer.from(
+        <string>appSecret?.body?.data?.orgName,
+        'base64'
+      ).toString('binary'),
+      repo: '.github',
+      title: taskName + ' failed',
+      body:
+        'Logs for `' +
+        taskName +
+        '`:\n' +
+        '```json\n' +
+        podObject.body +
+        '\n```',
+    });
+  } else {
+    log.info(taskName + ' succeeded.');
+  }
+};
+
 export default (
   app: Probot,
   {
@@ -84,6 +179,7 @@ export default (
         operation: 'k8s',
       })
       .inc();
+    return callback;
   };
 
   app.onAny((context: any) => {
@@ -124,19 +220,24 @@ export default (
     });
 
     // Trigger dump-config taskrun
-    wrapOperationWithMetrics(
-      useApi(APIS.CustomObjectsApi).createNamespacedCustomObject(
-        'tekton.dev',
-        'v1beta1',
-        getNamespace(),
-        'taskruns',
-        generateTaskPayload('peribolos-dump-config', context)
-      ),
-      {
-        install: context.payload.installation.id,
-        method: 'scheduleDumpConfig',
-      }
+    const taskName = await getTaskName(
+      wrapOperationWithMetrics(
+        useApi(APIS.CustomObjectsApi).createNamespacedCustomObject(
+          'tekton.dev',
+          'v1beta1',
+          getNamespace(),
+          'taskruns',
+          generateTaskPayload('peribolos-dump-config', context)
+        ),
+        {
+          install: context.payload.installation.id,
+          method: 'scheduleDumpConfig',
+        }
+      )
     );
+    await checkTaskStatus(taskName, app.log).then((failed) => {
+      handleTaskRunResult(<boolean>failed, taskName, app.log, context);
+    });
   });
 
   app.on('push', async (context: any) => {
@@ -165,20 +266,25 @@ export default (
       method: 'updateSecret',
     });
 
-    // Trigger taskrun to apply config changes to org
-    wrapOperationWithMetrics(
-      useApi(APIS.CustomObjectsApi).createNamespacedCustomObject(
-        'tekton.dev',
-        'v1beta1',
-        getNamespace(),
-        'taskruns',
-        generateTaskPayload('peribolos-run', context)
-      ),
-      {
-        install: context.payload.installation.id,
-        method: 'schedulePushTask',
-      }
+    const taskName = await getTaskName(
+      wrapOperationWithMetrics(
+        useApi(APIS.CustomObjectsApi).createNamespacedCustomObject(
+          'tekton.dev',
+          'v1beta1',
+          getNamespace(),
+          'taskruns',
+          generateTaskPayload('peribolos-run', context)
+        ),
+        {
+          install: context.payload.installation.id,
+          method: 'schedulePushTask',
+        }
+      )
     );
+
+    await checkTaskStatus(taskName, app.log).then((failed) => {
+      handleTaskRunResult(<boolean>failed, taskName, app.log, context);
+    });
   });
 
   app.on('installation.deleted', async (context: any) => {
